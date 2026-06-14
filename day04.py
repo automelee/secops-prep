7161fc24095c88344e17f2e69a64b44829a08140
automelee <eric87@gmail.com>
2026-06-14 00:04:09 -0400

    4일차 컨디셔널 파이썬

---
+++ day04.py
@@ -0,0 +1,10 @@
+forwarder_name = "melee1"
+log_count = 2511232
+threshold = 100
+
+if log_count == 0:
+    print ("Crit Crit this node is silent")
+elif log_count < 100:
+    print ("Warning, log count is being sketchy")
+else:
+    print ("Normal Operation")
